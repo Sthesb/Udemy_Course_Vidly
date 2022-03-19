c40c80de4a77c63b54b504dbb5c0506b9bc50562
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-19 21:39:57 +0200

    Movie, Customer and RandomMovieView Models

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,15 @@ namespace UdemyVidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //routes.MapRoute(
+            //    name: "MoviesByReleaseDate",
+            //    url: "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action="ByReleasedDate"},
+            //    new { year = @"\d{4}", month = @"\d{2}" }
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyVidly.Models;
+using UdemyVidly.ViewModels;
+
+namespace UdemyVidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "MovieName";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        // GET: movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { MovieName = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { CustomerName = "Sthembiso"},
+                new Customer { CustomerName = "Goodness"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+
+        }
+        public ActionResult Edit(int id)
+        {
+            
+            return Content("id " + id);
+        }
+
+
+        
+    }
+}
