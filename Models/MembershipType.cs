95bd73b43041a7256510e855170bf23739773592
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-21 14:55:06 +0200

    Membership name

--- Models/MembershipType.cs
+++ Models/MembershipType.cs
@@ -8,9 +8,11 @@ namespace UdemyVidly.Models
     public class MembershipType
     {
         public byte Id { get; set; }
+
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
         public byte DiscountRate { get; set; }
+        public string MembershipName { get; set; }
 
     }
 }
