a6562b32da26b145baab4c5e7945fc2f17a60dff
Sthembiso Mathebula <Sthembiso.Mathebula@gijima.com>
2022-03-22 23:42:13 +0200

    Add customer to database

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -5,6 +5,7 @@ using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using UdemyVidly.Models;
+using UdemyVidly.ViewModels;
 
 namespace UdemyVidly.Controllers
 {
@@ -26,21 +27,42 @@ namespace UdemyVidly.Controllers
 
 
 
-        // GET: Costomers
+        // GET: customers
         public ActionResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType ).ToList();
             return View(customers);
         }
 
-        // GET: Customer/Details/{id}
+        // GET: customer/Details/{id}
         public ActionResult Details(int id)
         {
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.CustomerId == id);
             return View(customer);
         }
 
+        // GET: customers/create
+        public ActionResult Create()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View(viewModel);
+        }
 
-        
+
+        // POST: customers/store
+        [HttpPost]
+        public ActionResult Store(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+
     }
 }
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -11,10 +11,13 @@ namespace UdemyVidly.Models
         public int CustomerId { get; set; }
         [Required]
         [StringLength(255)]
+        [Display(Name = "Name")]
         public string CustomerName { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
+        [Display (Name ="Date of Birth")]
         public DateTime ? BirthDate { get; set; }
     }
 }
